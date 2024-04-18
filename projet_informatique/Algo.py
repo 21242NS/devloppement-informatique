3e49bbd24c5c25bbcd1451c5d14ceb8914a906a6
21242NS <nicolasschell234@gmail.com>
2024-04-18 11:39:24 +0200

    Commit : Algo.py : finalisation des 4 premieres fct avec evaluate_board, peut_passer, peut_mettre_blockeurs et generate_moves. Pour la partie test je commit les tests des trois premieres fct

--- projet_informatique/Algo.py
+++ projet_informatique/Algo.py
@@ -7,13 +7,22 @@ sortie :
 import copy
 
 # Définition des constantes pour les valeurs du plateau
-PAWN1 = 0
-PAWN2 = 1
-EMPTY_PAWN = 2
-EMPTY_BLOCKER = 3
-BLOCKER = 4
-IMP = 5
-
+PAWN1 = 0.0
+PAWN2 = 1.0
+EMPTY_PAWN = 2.0
+EMPTY_BLOCKER = 3.0
+BLOCKER = 4.0
+IMP = 5.0
+Blockers = 10
+class Blocker():
+    def __init__(self, position):
+        self.position = position
+    def space1(self):
+        pos1=self.position[0]
+        return pos1
+    def space2(self):
+        pos2 =self.position[1]
+        return pos2
 # Fonction pour évaluer la position actuelle du plateau
 def evaluate_board(board, pawn):
     position = [0,0]
@@ -29,20 +38,68 @@ def peut_passer(board, pos_depart, pos_final):
     case_a_checker_x = 0
     case_a_checker_y = 0
     for i in range(2) :
-        if i == 0 :
-            case_a_checker_x = (pos_depart[i] - pos_final[i])/2+pos_depart[i]
+        if pos_final[i]-pos_depart[i]==0 or pos_final[i]-pos_depart[i] ==2 or pos_final[i]-pos_depart[i]==-2 :
+            if i == 0 :
+                case_a_checker_x = int((pos_final[i] - pos_depart[i])/2+pos_depart[i])
+            else :
+                case_a_checker_y = int((pos_final[i] - pos_depart[i])/2+pos_depart[i])
         else :
-            case_a_checker_y = (pos_depart[i] - pos_final[i])/2+pos_depart[i]
-    if board[case_a_checker_x][case_a_checker_y]==3.0 :
-        return True 
-    else : 
+            return False
+    if case_a_checker_x>=len(board) or case_a_checker_y>=len(board):
         return False
-
+    elif board[case_a_checker_x][case_a_checker_y]== EMPTY_BLOCKER :
+        return True 
+    else :
+        return False
+def peut_mettre_blockeurs(board, pos_blocker1, pos_blocker2):
+    case_mid_x = 0
+    case_mid_y =0
+    for i in range(len(pos_blocker1)) :
+        if pos_blocker1[i]-pos_blocker2[i]==0 or pos_blocker1[i]-pos_blocker2[i] ==2 or pos_blocker1[i]-pos_blocker2[i]==-2 :
+            res = True
+        else :
+            return False
+    if board[pos_blocker1[0]][pos_blocker1[1]] == EMPTY_BLOCKER and board[pos_blocker2[0]][pos_blocker2[1]]== EMPTY_BLOCKER :
+        case_mid_x = int((pos_blocker1[0]-pos_blocker2[0])/2+pos_blocker2[0])
+        case_mid_y=int((pos_blocker1[1]-pos_blocker2[1])/2+pos_blocker2[1])
+        if board[case_mid_x][case_mid_y]==EMPTY_PAWN or board[case_mid_x][case_mid_y] == PAWN1 or board[case_mid_x][case_mid_y] == PAWN2 or board[case_mid_x][case_mid_y] == BLOCKER :
+            return False
+        else :
+            return True
+    else :
+        return False
 # Fonction pour générer tous les coups possibles à partir d'une position donnée
-def generate_moves(board, pos, pawn):
-    moves = []
-    for 
-    return moves
+def generate_moves(board, pawn):
+    b_move= []
+    b_move_a=[]
+    #part for the pawn:
+    pos = evaluate_board(board, pawn)
+    pos_up= [pos[0]-2,pos[1]]
+    pos_down=[pos[0]+2,pos[1]]
+    pos_left = [pos[0],pos[1]-2]
+    pos_right=[pos[0],pos[1]+2]
+    p_moves_b = [pos_up,pos_down,pos_left,pos_right]
+    p_moves_a = []
+    for i in range(len(p_moves_b)) :
+        if peut_passer(board,pos,p_moves_b[i]):
+            p_moves_a.append(p_moves_b[i])
+    #part for the blockers :
+    for i in range(1,len(board),2):
+            for j in range(0,len(board[i]), 2) :
+                x = j+2
+                if x<len(board):
+                    b_move.append([[j,i],[x,i]])
+    for i in range(1,len(board),2):
+            for j in range(0,len(board[i]), 2) :
+                x = j+2
+                if x<len(board):
+                    b_move.append([[i,j],[i,x]])
+    for i in range(len(b_move)):
+        if peut_mettre_blockeurs(board,b_move[i][0],b_move[i][1]):
+            b_move_a.append(b_move[i])
+    all_moves=[p_moves_a,b_move_a]
+    
+    return all_moves
 
 # Fonction pour effectuer un coup sur le plateau
 def make_move(board, move):
@@ -82,7 +139,25 @@ def choose_move(board):
     return best_move
 
 # Exemple d'utilisation
-board = init_board()
-print_board(board)
-ai_move = choose_move(board)
-print("AI's move:", ai_move)
+#board = init_board()
+#print_board(board)
+#ai_move = choose_move(board)
+#print("AI's move:", ai_move)
+board =     [[2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 0.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0],
+             [3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 4.0, 5.0, 4.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0],
+             [2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0],
+             [3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0],
+             [2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0],
+             [3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0],
+             [2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0],
+             [3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0],
+             [2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0],
+             [3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0],
+             [2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0],
+             [3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0],
+             [2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0],
+             [3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0],
+             [2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0],
+             [3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0],
+             [2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 1.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0, 3.0, 2.0]]
+print(generate_moves(board,PAWN2))
